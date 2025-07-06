e7a9a8ffc9f6743a4a93d23508159b5363898709
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-07-06 08:12:16 +0700

    fix: update allow origin

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	// NOTE - Use cors
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000, http://localhost:3001, https://belugatasks.dev, https://www.belugatasks.dev",
+		AllowOrigins: "*",
 		AllowMethods: "GET,POST,PUT,PATCH,DELETE",
 		AllowHeaders: "Content-Type,Authorization",
 		AllowCredentials: true,
